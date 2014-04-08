5fd1b31ddb872d5b47985bd7f89e490654cd7d16
Brandon West <bwest1711@gmail.com>
2014-04-08 11:08:12 -0400

    added all pictures for donation page

--- images.php
+++ images.php
@@ -8,7 +8,17 @@
         <?php include('includes/header.php') ?>
         
         <div class="polaroid-images">
-            <a href="" title="one"><img height="200" src="images/slideshow/image_1.png" alt="one" title="one" /></a>
+            <a href="" title="Abrigail"><img height="200" src="images/donate/abigail.png" alt="Abrigail" title="Abrigail" /></a>
+            <a href="" title="Anna"><img height="200" src="images/donate/anna.png" alt="Anna" title="Anna" /></a>
+            <a href="" title="Annabelle"><img height="200" src="images/donate/annabelle.png" alt="Annabelle" title="Annabelle" /></a>
+            <a href="" title="April"><img height="200" src="images/donate/april.png" alt="April" title="April" /></a>
+            <a href="" title="David"><img height="200" src="images/donate/david.png" alt="David" title="David" /></a>
+            <a href="" title="Jacob"><img height="200" src="images/donate/jacob.png" alt="Jacob" title="Jacob" /></a>
+            <a href="" title="Jenn"><img height="200" src="images/donate/jenn.png" alt="Jenn" title="Jenn" /></a>
+            <a href="" title="Kelsey"><img height="200" src="images/donate/kelsey.png" alt="Kelsey" title="Kelsey" /></a>
+            <a href="" title="Patricia"><img height="200" src="images/donate/patricia.png" alt="Patricia" title="Patricia" /></a>
+            <a href="" title="Sara"><img height="200" src="images/donate/sara.png" alt="Sara" title="Sara" /></a>
+            
             
         </div>
     </body>
