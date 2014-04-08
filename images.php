e1f169091254057f49d2dab97d95ed03aa431119
Brandon West <bwest1711@gmail.com>
2014-04-08 10:53:48 -0400

    Polaroid image css
    Included css to display images as polaroids.

---
+++ images.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>K94T1</title>
+        <?php include('includes/head.php') ?>
+    </head>
+    <body>
+        <?php include('includes/header.php') ?>
+        
+        <div class="polaroid-images">
+            <a href="" title="one"><img height="200" src="images/slideshow/image_1.png" alt="one" title="one" /></a>
+            
+        </div>
+    </body>
+</html>
